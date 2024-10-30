6a66b80d6fcbe04c5b91669b9595f3fe4049abb8
unknown <leonard.matei2011@yahoo.com>
2024-10-30 17:28:35 +0200

    some style

--- frontend/src/components/projectsList/index.jsx
+++ frontend/src/components/projectsList/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import './styles.scss';
 
 const fetchProjects = async () => {
   const response = await axios.get('http://localhost:3000/api/projects');
@@ -21,28 +22,37 @@ const ProjectsList = () => {
   const handleEdit = (project) => {
     navigate(`/project/${project.id}`);
   };
+  const handleDelete = (prpject) => {};
 
   return (
-    <ul>
-      {data.map((project) => (
-        <li key={project.id}>
-          <h2>{project.title}</h2>
-          <p>{project.description}</p>
-          <a href={project.link} target='_blank' rel='noopener noreferrer'>
-            View Project
-          </a>
-          {!!project.images.length &&
-            project.images.map((item) => (
-              <img
-                key={`image_id${item.id}, project id${project.id}`}
-                src={item.image_data}
-                alt={`image id:${item.id} from project id:${project.id}`}
-              ></img>
-            ))}
-          <button onClick={() => handleEdit(project)}>Edit Project</button>{' '}
-        </li>
-      ))}
-    </ul>
+    <div className='home-page'>
+      <div className='modal-overlay'>
+        <div className='modal'>modal</div>
+      </div>
+      <ul>
+        {data.map((project) => (
+          <li key={project.id}>
+            <h2>{project.title}</h2>
+            <p>{project.description}</p>
+            <a href={project.link} target='_blank' rel='noopener noreferrer'>
+              View Project
+            </a>
+            {!!project.images.length &&
+              project.images.map((item) => (
+                <img
+                  key={`image_id${item.id}, project id${project.id}`}
+                  src={item.image_data}
+                  alt={`image id:${item.id} from project id:${project.id}`}
+                ></img>
+              ))}
+            <button onClick={() => handleEdit(project)}>Edit Project</button>
+            <button onClick={() => console.log('deleted')}>
+              Delete Project
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
